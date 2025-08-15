7fbbebf935b0f61f5e63e3d704cebd4ee2ae9954
Denis <dvavramenko@ispras.ru>
2025-08-15 11:54:51 +0300

    Separated by interfaces and add example of sequence

--- vending_machine.sv
+++ vending_machine.sv
@@ -1,41 +1,29 @@
 
 
-//Or should I make with parametr 
-interface vending_machine(input bit clk, input bit rst_n );
-   // Input
-   struct packed{
+interface data_in_if(input bit clk, input bit rst_n );
      logic [31:0] regs_data_in;
-     logic [31:0] regs_data_out;
      logic regs_we;
      logic [7:0] regs_addr;
-   } data_in  
-   
+endinterface
 
-   struct packed {
-       logic id_valid;
-       logic [8:0] client_id;
-       logic [5:0] coin_in;
-       logic [1:0] currency_type;
-       logic coin_insert;
-       logic [30:0] item_select; // [04:00] num_items -> max(num_items) = 31
-       logic confirm;
-   } user_in;
+interface user_in_if(input bit clk, input bit rst_n );
+    logic [8:0] client_id;
+    logic [5:0] coin_in;
+    logic [1:0] currency_type;
+    logic [30:0] item_select; // [04:00] num_items -> max(num_items) = 31
+endinterface
 
+interface admin_in_if(input bit clk, input bit rst_n );
+    logic [31:0] admin_password;
+endinterface
 
-   struct packed {
-       logic admin_mode;
-       logic [31:0] admin_password;
-   } admin_in;
+interface security_in_if(input bit clk, input bit rst_n );
+    logic tamper_detect;
+    logic jam_detect;
+    logic power_loss;
+endinterface
 
-   struct packed {
-       logic tamper_detect;
-       logic jam_detect;
-       logic power_loss;
-   } security_in;
-
-
-   //Output
-   struct packed {
+interface data_out_if(input bit clk, input bit rst_n );
      logic access_error;
      logic [30:0] item_out; // [04:00] num_items -> max(num_items) = 31
      logic [5:0] change_out;
@@ -44,49 +32,96 @@ interface vending_machine(input bit clk, input bit rst_n );
      logic [7:0] client_points;
      logic alarm;
      logic [2:0] state;
-   } data_out
-
-   clocking master_cb @ ( posedge clk );
-      default input #1step output #1ns;
-      output  data_in, user_in, admin_in, security_in;
-      input data_out;
-   endclocking: master_cb
-
-   clocking slave_cb @ ( posedge clk );
-      default input #1step output #1ns;
-      input  data_in, user_in, admin_in, security_in;
-      output data_out;
-   endclocking: slave_cb
-   
-   modport master_mp (input clk, rst_n, output data_in, user_in, admin_in, security_in, input  data_out);
-
-   modport slave_mp (input clk, rst_n, input  data_in, user_in, admin_in, security_in, output data_out);
-
-   modport master_sync_mp (clocking master_cb);
-   modport slave_sync_mp (clocking slave_cb, input rst_n);
- endinterface: jelly_bean_if
+endinterface
 
 
-package machine_env;
-   import uvm_pkg::*;
-   `include "uvm_macros.svh"
+package vending_machine
+    import uvm_pkg::*;
+    include "uvm_macros.svh";
     function random_range(int range_start = 0, int range_end = 1);
       int value;
       std::randomize(value) with {range_start <= value; value <= range_end; };
       return value;
-    endfunction: random_range
+    endfunction
+
+    typedef enum {CUR_RUB=2'b00, CUR_USD=2'b01, CUR_EUR=2'b10} money_type;
+
+    class change_data_in  extends uvm_sequence_item;;
+        logic [31:0] regs_data_in;
+        logic [31:0] regs_data_out;
+        logic regs_we;
+        logic [7:0] regs_addr;
+
+        `uvm_object_utils_begin(change_data_in)
+            `uvm_field_int(regs_data_in , UVM_ALL_ON)
+            `uvm_field_int(regs_we , UVM_ALL_ON)
+            `uvm_field_int(regs_addr, UVM_ALL_ON)
+            `uvm_field_int(regs_data_out, UVM_DEFAULT)
+        `uvm_object_utils_end
+        function new(string name = "change_data_in");
+            super.new(name);
+        endfunction
+
+        function void randomize();
+            regs_data_in = random_range(0, ~32'd0); 
+            regs_we = random_range(0, 1);
+            regs_addr = random_range(0, ~8'd0);
+        endfunction
+
+    endclass  
+
+    class item_buy_seq extends uvm_sequence_item;
+        logic [8:0] client_id;
+        logic [5:0] coin_in;
+        money_type currency_type;
+        logic [30:0] item_select; // constraints on super class (?) or in .svh
+        function new( string name = "" );
+            super.new( name );
+            this.m_random();
+        endfunction
+
+        function void m_random();
+            client_id = random_range(0,  ~9'd0); 
+            coin_in = random_range(0,  ~5'd0); 
+            currency_type = money_type'(random_range(0, 2)); 
+            item_select  = (31'd1) << random_range(0, 30);
+        endfunction: m_random
+
+      `uvm_object_utils_begin(item_buy_seq)
+        `uvm_field_int(client_id    , UVM_ALL_ON)
+        `uvm_field_int(coin_in      , UVM_ALL_ON)
+        `uvm_field_enum(money_type, currency_type, UVM_ALL_ON)
+        `uvm_field_int(item_select  , UVM_ALL_ON)
+      `uvm_object_utils_end
+
+    endclass
+
+
+    class one_item_buy_seq extends uvm_sequence#( item_buy_seq );
+      `uvm_object_utils( one_item_buy_seq  )
+
+      function new( string name = "" );
+          super.new( name );
+      endfunction: new
+
+      task body();
+          item_buy_seq item_tx;
+          item_tx = item_buy_seq::type_id::create( .name( "item_tx" ) );
+          start_item( item_tx );
+          item_tx.m_random();
+          finish_item( item_tx );
+      endtask: body
+    endclass: one_jelly_bean_sequence
+
+    class many_item_buy_seq extends one_item_buy_seq; // or extends uvm_sequence#(one_item_buy_seq)
+        int unsigned item_num; // или в task body все генерировать
+
+      `uvm_object_utils_begin( many_item_buy_seq )
+          `uvm_field_int( item_num, UVM_ALL_ON )
+      `uvm_object_utils_end
+    endclass
+
+
+      
+endpackage 
   
-
-  class vending_machine_configuration extends uvm_object;
-   `uvm_object_utils( vending_machine_configuration )
-
-    function new( string name = "" );
-      super.new( name );
-    endfunction: new
-  endclass: vending_machine_configuration
-
-  class buying_transaction extends uvm_sequence_item;
-    
-  endclass buying_transaction;
-endpackage: machine_env
-
